68eacc76864360cdd7e7ab792f8dd301cdc982bb
Romain Prevost <romain.prevost@melusim.fr>
2023-01-19 21:54:52 +0100

    Seeder pour les articles avec tags et création d'un administrateur

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,18 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Création d'un administrateur
+        User::factory()->admin()->create([
+            'name' => 'Administrateur',
+            'email' => 'admin@example.com',
+        ]);
+
+        // Création de 50 articles, chacun avec 3 tags
+        Article::factory()
+            ->count(50)
+            ->has(
+                Tag::factory()->count(3)
+            )->create();
     }
 }
